5b1caf5190552bacd03949b9c44571f3ad85a92e
samuel-mfc <samuelalencarmfc@gmail.com>
2025-12-06 15:36:47 -0300

    Add Plotly Express import to streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import os
 import pandas as pd
 import numpy as np
 import streamlit as st
+import plotly.express as px
 
 # =========================================================
 # 1. Funções de processamento (adaptadas do seu script)
